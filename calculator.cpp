7b75bb389e6d5fce45349a60ca490ed5ec9f4283
Nikolay Glazov <glazov_n_o@mail.ru>
2020-12-05 01:03:03 +0300

    added tests (factorial for 1000, 2000, 3000, 4000)

--- calculator.cpp
+++ calculator.cpp
@@ -4,6 +4,7 @@
 #include <climits>
 #include <ctime>
 #include <iostream>
+#include <map>
 #include <string>
 
 //! преобразование строки в число
@@ -54,6 +55,7 @@ int main()
         while (std::cin.get() != '\n') continue;
     }
 #else
+#if 0
     expr.prepare("!(4000)");
     std::string result;
     const auto t1 = time(nullptr);
@@ -65,6 +67,26 @@ int main()
     const auto T = T2 - T1;
     std::cout << "Time=" << t << "\t" << T << "\t" << (float(T) / CLOCKS_PER_SEC) << std::endl;
     std::cout << "\nresult=" << result << std::endl;
+#else
+    constexpr size_t NDIGITS = 30;
+    const std::map<std::string, std::pair<std::string, size_t>> examples{
+        {"!(1000)", {"402387260077093773543702433923", 2568}},
+        {"!(2000)", {"331627509245063324117539338057", 5736}},
+        {"!(3000)", {"414935960343785408555686709308", 9131}},
+        {"!(4000)", {"182880195151406501331474317557", 12674}},
+    };
+    for (const auto &example : examples) {
+        const std::string a = example.first;
+        const std::string b = example.second.first;
+        const auto c = example.second.second;
+        expr.prepare(a);
+        const std::string x = expr.compute();
+        std::cout << x << std::endl;
+        std::cout << x.substr(0, NDIGITS) << "\t" << x.length() << std::endl;
+        std::cout << b << "\t" << c << std::endl;
+        std::cout << (x.substr(0, NDIGITS) == b) << "\t" << (x.length() == c) << std::endl;
+    }
+#endif
 #endif
     std::cout << "Press 'Enter'" << std::endl;
     std::cin.get();
